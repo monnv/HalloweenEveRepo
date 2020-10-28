e5a960b851053f89fbf5dbc5342a15168bb86d9f
Ami Cai <ami.cai333@gmail.com>
2020-10-28 13:30:13 -0400

    Thought Dialogue
    Code and Animation done!

---
+++ Assets/Scripts/ThoughtDialogue.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ThoughtDialogue : MonoBehaviour
+{
+    public TextMeshProUGUI textDisplay;
+    public string[] sentences;
+    private int index;
+    public float typingSpeed;
+
+    public GameObject thoughtPoint;
+
+    //public GameObject continueButton;
+
+
+    private void Update()
+    {
+        //if(textDisplay.text == sentences[index])
+        //{
+            //continueButton.SetActive(true);
+        //}
+    }
+
+    public void Typing()
+    {
+        StartCoroutine(Type());
+    }
+
+    IEnumerator Type()
+    {
+        yield return new WaitForSeconds(1f);
+        foreach (char letter in sentences[index].ToCharArray()){
+            textDisplay.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+    }
+
+    //Continue Button Bellow
+
+   // public void NextSentence()
+    //{
+        //continueButton.SetActive(false);
+
+        //if(index < sentences.Length - 1)
+        //{
+            //index++;
+            //textDisplay.text = "";
+            //StartCoroutine(Type());
+        //}
+        //else
+        //{
+           // textDisplay.text = "";
+            //continueButton.SetActive(false);
+        //}
+    //}
+}
