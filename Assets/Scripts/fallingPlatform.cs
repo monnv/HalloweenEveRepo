5f1246670aee5f9dc264e754cae6deaa1a8370c8
Ami Cai <ami.cai333@gmail.com>
2020-10-28 04:48:51 -0400

    Falling Platforms

---
+++ Assets/Scripts/fallingPlatform.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fallingPlatform : MonoBehaviour
+{
+
+    public Rigidbody2D rb2d;
+    Vector2 startPos;
+
+    public float fallDelay;
+
+    public bool respawns = true;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            StartCoroutine(Fall());
+
+        }
+    }
+
+    IEnumerator Fall()
+    {
+        yield return new WaitForSeconds(fallDelay);
+        rb2d.isKinematic = false;
+        Destroy(gameObject, 2f);
+    }
+}
