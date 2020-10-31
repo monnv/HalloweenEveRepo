1a6b47cdc3d7d5edd6198876fdedc07f316464d3
Ami Cai <ami.cai333@gmail.com>
2020-10-31 16:22:18 -0400

    THEYRE TALKING

---
+++ Assets/Scripts/Lv 6 Convo/SpeechDialogue1.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class SpeechDialogue1 : MonoBehaviour
+{
+    public TextMeshProUGUI textDisplay;
+    public string[] sentences;
+    private int index;
+    public float typingSpeed;
+
+    public Animator appearAnimation;
+
+    //private void Update()
+    //{
+        //if (textDisplay.text == sentences[index])
+        //{
+            //if (Input.GetKeyDown(KeyCode.Space))
+            //{
+                //NextSentence();
+            //}
+        //}
+    //}
+
+    public void Speak()
+    {
+        StartCoroutine(Type());
+    }
+
+    IEnumerator Type()
+    {
+        yield return new WaitForSeconds(1f);
+        foreach (char letter in sentences[index].ToCharArray())
+        {
+            textDisplay.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+        
+    }
+
+    public void NextSentence()
+    {
+        if(index < sentences.Length - 1)
+        {
+            index++;
+            textDisplay.text = "";
+            StartCoroutine(Type());
+        }
+        else
+        {
+            textDisplay.text = "";
+        }
+    }
+}
+
---
+++ Assets/Scripts/TimelineManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class TimelineManager : MonoBehaviour
+{
+    private bool fix = false;
+
+    public PlayableDirector director;
+
+    public Animator playerAnimator;
+    public RuntimeAnimatorController playerAnim;
+
+    void OnEnable()
+    {
+        playerAnim = playerAnimator.runtimeAnimatorController;
+        playerAnimator.runtimeAnimatorController = null;
+    }
+
+    private void Update()
+    {
+        if (director.state != PlayState.Playing && !fix)
+        {
+            fix = true;
+            playerAnimator.runtimeAnimatorController = playerAnim;
+        }
+    }
+}
