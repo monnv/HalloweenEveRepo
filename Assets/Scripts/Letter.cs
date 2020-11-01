576f7397e33df097031b9d6da9b3b8c7d20d64df
Ami Cai <ami.cai333@gmail.com>
2020-11-01 00:57:43 -0500

    check in

---
+++ Assets/Scripts/Letter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Letter : MonoBehaviour
+{
+    public float time;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Leggo());
+    }
+
+    IEnumerator Leggo()
+    {
+        yield return new WaitForSeconds(time);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
--- Assets/Scripts/TalkPoint.cs
+++ Assets/Scripts/TalkPoint.cs
@@ -6,11 +6,13 @@ public class TalkPoint : MonoBehaviour
 {
 
     public GameObject gameObject;
+    public GameObject Convo;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
+            Convo.SetActive(true);
             FindObjectOfType<SpeechDialogue>().Speak();
             gameObject.SetActive(false);
         }
